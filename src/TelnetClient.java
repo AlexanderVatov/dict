2f849d9d5da3e0b1c697dd4974ffc71115a7ab4d
Alexander Vatov <alexander.vatov@uni.lu>
2024-03-28 21:37:44 +0100

    Add basic Telnet client implementation

--- src/TelnetClient.java
+++ src/TelnetClient.java
@@ -1,6 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class TelnetClient {
-    public String readLine() {
-        return "";
+    private Socket socket;
+    private PrintWriter outbound;
+    private BufferedReader inbound;
+
+    TelnetClient(String hostname, int port) throws IOException {
+        socket = new Socket(hostname, port);
+        outbound = new PrintWriter(socket.getOutputStream());
+        inbound = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public String readLine() throws IOException {
+        String result;
+        //Get rid of any initial blank lines
+        while (true){
+            result = inbound.readLine();
+            if(result != null && result.isEmpty())
+                continue;
+            else
+                return result;
+        }
+    }
+
+    public boolean ready() throws IOException {
+        return inbound.ready();
+    }
+
+    public void writeLine(String line) {
+        outbound.println(line);
+        outbound.flush();
+    }
+
+    public void close() throws IOException {
+        outbound.close();
+        inbound.close();
+        socket.close();
     }
 }
