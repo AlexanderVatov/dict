fc13c956f2d9c5a7217d1a9aa48e2b3be6b7cd0a
Alexander Vatov <alexander.vatov@uni.lu>
2024-04-01 20:26:50 +0200

    Add unit test

---
+++ test/TCPClientTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TCPClientTest {
+    @Test
+    public void testOnline() throws IOException {
+        TCPClient client = new TCPClient("india.colorado.edu", 13);
+        String response = client.readLine() + "\n" + client.readLine();
+        assertTrue(response.contains("UTC(NIST)"));
+    }
+}
