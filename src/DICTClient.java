9c7bf1fb528adf3a865b039c9e1fd5051fee9168
Alexander Vatov <1920061+AlexanderVatov@users.noreply.github.com>
2024-04-06 20:22:00 +0200

    Handle "." delimiters correctly in DICTClient.define

--- src/DICTClient.java
+++ src/DICTClient.java
@@ -8,20 +8,22 @@ public class DICTClient {
     public interface Backend {
         String query(String query) throws IOException;
     }
-    private Backend backend;
+    protected final Backend backend;
 
     public DICTClient(Backend backend){
         this.backend = backend;
     }
     public record Definition(String headword, String dictionaryID, String dictionaryName, String text) {}
+    public record Definition(String headword, String databaseID, String databaseDescription, String text) {}
 
-    public List<Definition> define(String dictionaryID, String query) throws IOException {
-        String response = backend.query(STR."DEFINE \{dictionaryID} \{query}");
+    public List<Definition> define(String databaseID, String query) throws IOException {
+        // The format of the command and the response are described in section 3.2 of RFC 2229:
+        // https://www.rfc-editor.org/rfc/rfc2229.html#section-3.2
+        String response = backend.query(STR."DEFINE \{databaseID} \{query}");
         if(response == null || response.isBlank()) {
             throw new IOException("Backend gave no response or empty response!");
         }
         Scanner scanner = new Scanner(response.strip());
-
 
         int code = scanner.nextInt();
         if(code == 220) {
@@ -29,6 +31,8 @@ public class DICTClient {
             code = scanner.nextInt();
         }
         List<Definition> definitionList = new ArrayList<>();
+
+        //Possible codes and syntax: https://www.rfc-editor.org/rfc/rfc2229.html#section-3.2.2
         int numberOfDefinitionsExpected = 0;
         if(code == 150) numberOfDefinitionsExpected = scanner.nextInt();
         else if(code == 552) {
@@ -36,26 +40,32 @@ public class DICTClient {
             return definitionList;
         } else if (code == 550) {
             //Invalid database
-            throw new IllegalArgumentException(String.format("No such dictionary ID: \"%s\"",dictionaryID));
+            throw new IllegalArgumentException(String.format("No such database ID: \"%s\"",databaseID));
         }
         scanner.nextLine();
 
         //Start parsing definitions
         Pattern defaultDelimiter = scanner.delimiter();
-        Pattern endOfDefinition = Pattern.compile("\\R(?=[0-9]{3})");
+        Pattern endOfDefinition = Pattern.compile("\\R[.]\\R(?=[0-9]{3})");
         Pattern quotedString = Pattern.compile("\".+?\"");
 
         for(int i = 0; i < numberOfDefinitionsExpected; ++i) {
             if(scanner.nextInt() != 151) System.err.println("Warning: Code 151 expected at the start of a definition!");
             String headword = scanner.next(quotedString).replace("\"","");
-            String readDictionaryID = scanner.next();
-            String dictionaryName = scanner.nextLine().replace("\"","").strip();
+            String readDatabaseID = scanner.next();
+            String databaseName = scanner.nextLine().replace("\"","").strip();
 
+            // Per section 2.4.3 of RFC 2229, each definition ends with a line containing
+            // a single period (\n.\n):
+            // https://www.rfc-editor.org/rfc/rfc2229.html#section-2.4.3
             scanner.useDelimiter(endOfDefinition);
             String definitionText = scanner.next();
             scanner.useDelimiter(defaultDelimiter);
+            // Get rid of the final dot
+            scanner.next();
 
-            definitionList.add(new Definition(headword,readDictionaryID,dictionaryName,definitionText));
+            Definition definition = new Definition(headword, readDatabaseID, databaseName, definitionText);
+            definitionList.add(definition);
         }
         return definitionList;
     }
